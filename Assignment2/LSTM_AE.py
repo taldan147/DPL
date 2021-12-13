406589f04d78ac59342269caedf9dca621e9215f
itamarkedem-student <itamarked@wix.com>
2021-12-13 12:21:50 +0200

    the base of the NN

--- Assignment2/LSTM_AE.py
+++ Assignment2/LSTM_AE.py
@@ -12,3 +12,34 @@ class Encoder(nn.Module):
 
     def forward(self, x):
         output, (h_t, c_t) = self.lstmEncoder(x)
+        return h_t.view(-1, 1, self.num_of_layers)
+
+class Decoder(nn.Module):
+    def __init__(self, input_size, num_of_layers, hs_size, dropout):
+        super(Decoder, self).__init__()
+        self.input_size = input_size
+        self.num_of_layers = num_of_layers
+        self.hs_size = hs_size
+        self.dropout = dropout
+        self.linear = nn.Linear(hs_size, input_size)
+        self.lstmDecoder = nn.LSTM(input_size, hs_size, num_of_layers, batch_first=True, dropout=dropout)
+
+    def forward(self, x: torch.tensor):
+        x = x.repeat(1, self.input_size)
+        output, (_, _) = self.lstmDecoder(x)
+        return self.linear(output)
+
+
+class LSTMAE(nn.Module):
+    def __init__(self, input_size, num_of_layers, hs_size, dropout):
+        super(LSTMAE, self).__init__()
+        self.input_size = input_size
+        self.num_of_layers = num_of_layers
+        self.hs_size = hs_size
+        self.dropout = dropout
+        self.encoder = Encoder(input_size, num_of_layers, hs_size, dropout)
+        self.decoder = Decoder(input_size, num_of_layers, hs_size, dropout)
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        return self.decoder(encoded)
