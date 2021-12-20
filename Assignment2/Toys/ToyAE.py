1af2efc8fc9079a5b6846ac3a31a5ac9d64692e9
Tal Dan <tdan@salesforce.com>
2021-12-20 16:11:04 +0200

    started finish Toy

--- Assignment2/Toys/ToyAE.py
+++ Assignment2/Toys/ToyAE.py
@@ -11,25 +11,28 @@ import time
 
 parser = argparse.ArgumentParser(description="Arguments of Toy AE")
 parser.add_argument('--batch_size', type=int, default=128, help="batch size")
-parser.add_argument('--epochs', type=int, default=200, help="number of epochs")
+parser.add_argument('--epochs', type=int, default=100, help="number of epochs")
 parser.add_argument('--optimizer', default='Adam', type=str, help="optimizer to use")
-parser.add_argument('--hidden_size', type=int, default=50, help="lstm hidden size")
+parser.add_argument('--hidden_size', type=int, default=20, help="lstm hidden size")
 parser.add_argument('--num_of_layers', type=int, default=3, help="num of layers")
 parser.add_argument('--lr', type=float, default=0.001, help="learning rate")
 parser.add_argument('--input_size', type=int, default=1, help="size of an input")
 parser.add_argument('--dropout', type=float, default=0.2, help="dropout ratio")
 parser.add_argument('--seq_size', type=int, default=50, help="size of a seq")
 parser.add_argument('--output_size', type=int, default=1, help="size of the output")
+parser.add_argument('--grad_clip', type=int, default=1, help="gradient clipping value")
 args =  parser.parse_args()
 
 class ToyAE():
     def __init__(self):
         super(ToyAE, self).__init__()
         data = Gen.genData()
-        self.trainData = data[:int(0.6 * len(data)), :]
-        self.validateData = data[int(0.6 * len(data)):int(0.8 * len(data)), :]
+        self.trainData = data[:6000]
+        self.validateData = data[6000:8000]
+        self.testData = data[8000:]
         self.epochs = args.epochs
         self.batchs = args.batch_size
+        self.grad_clip  =args.grad_clip
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.AE = AE.LSTMAE(args.input_size, args.num_of_layers, args.seq_size, args.hidden_size, args.dropout, args.output_size)
         self.optimizer = torch.optim.Adam(self.AE.parameters(), args.lr) if (args.optimizer == "Adam")  else torch.optim.SGD(self.AE.parameters(), lr=args.lr)
@@ -53,11 +56,15 @@ class ToyAE():
                 output = self.AE.forward(currX)
                 loss = nn.MSELoss().forward(output, currX)
                 loss.backward()
+                if self.grad_clip is not None:
+                    torch.nn.utils.clip_grad_norm(self.AE.parameters(), self.grad_clip)
                 self.optimizer.step()
                 currLoss += loss.item()
-            avgLoss = currLoss / len(self.trainData)
+            avgLoss = currLoss / (math.floor(len(self.trainData)/self.batchs))
+            currLoss = nn.MSELoss().forward(self.AE(self.validateData), self.validateData)
+            validateLoss.append(currLoss.item())
             trainLoss.append(avgLoss)
-        return trainLoss
+        return trainLoss, validateLoss
 
 
     def reconstruct(self, data):
@@ -67,10 +74,12 @@ class ToyAE():
     def plotNN(self):
 
         startLoss = time.perf_counter()
-        loss = self.train()
+        trainLoss, validLoss = self.train()
         plt.figure()
-        plt.title("Loss on Toys")
-        plt.plot(np.arange(self.epochs), loss)
+        plt.title("Loss of train and validation - Toys")
+        plt.plot(np.arange(self.epochs), trainLoss, label='train')
+        plt.plot(np.arange(self.epochs), validLoss, label='validation')
+        plt.legend()
         plt.show()
 
         endLoss = time.perf_counter()
@@ -91,4 +100,4 @@ class ToyAE():
         print(f"the reconstruct calc took {(endReconstruct-endLoss)/60} minutes")
         print(f"overall it took {(endReconstruct-startLoss)/60} minutes")
 
-# ToyAE().plotNN()
+ToyAE().plotNN()
